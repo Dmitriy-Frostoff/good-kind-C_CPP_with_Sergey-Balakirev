ea659133c313a976a5b50dfca919149cf3970874
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-29 20:59:24 +0300

    chore(tasks): update 2.2.2 task description

--- tasks/2. Variables and arithmetic operations/2.2 Assignment, numeric and symbolic literals, sizeof/Feat 02/main.c
+++ tasks/2. Variables and arithmetic operations/2.2 Assignment, numeric and symbolic literals, sizeof/Feat 02/main.c
@@ -1,6 +1,10 @@
 /**
  * The task description is at
  * @link{https://stepik.org/lesson/1191244/step/4?unit=1204215}
+ *
+ * Declare an integer variable of type 'int' named 'my_var_i' and assign it the
+ * value 78
+ *
  */
 int main(void) {
   int my_var_i = 78;
