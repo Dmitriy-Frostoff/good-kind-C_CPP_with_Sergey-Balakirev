a645a9429fd89b6e72e1a640b90d895d58ed2a42
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-29 19:51:03 +0300

    feat(tasks): add 2.2.2 task; chore(1.4 task structure): update folder structure due to course one

---
+++ tasks/2. Variables and arithmetic operations/2.2 Assignment, numeric and symbolic literals, sizeof/Feat 02/main.c
@@ -0,0 +1,13 @@
+/**
+ * The task description is at
+ * @link{https://stepik.org/lesson/1191244/step/4?unit=1204215}
+ */
+int main(void) {
+  int my_var_i = 78;
+  /*
+   * макроопределение для тестирования
+   * (не убирать и должно идти непосредственно перед return 0)
+   */
+  __ASSERT_TESTS__
+  return 0;
+}
