aad98addc15a785fdc9f0bbff5d6778523063b5b
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-29 20:26:13 +0300

    feat(task): add 2.4.7 task

---
+++ tasks/2. Variables and arithmetic operations/2.4 Printf() function for formatted output/Feat 07/main.c
@@ -0,0 +1,15 @@
+/**
+ * The task description is at
+ * @link{https://stepik.org/lesson/1191247/step/9?unit=1204218}
+ */
+#include <stdio.h>
+
+int main(void) {
+  int a1 = -123;
+  int a2 = 6;
+  int a3 = 1024;
+
+  printf("[%5d]\n[%5d]\n[%5d]\n", a1, a2, a3);
+
+  return 0;
+}
