25b498fab546b25880fb796f1001a43f89e11aec
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-29 21:12:00 +0300

    chore(tasks): update 2.3.5 task description and tests data

--- tasks/2. Variables and arithmetic operations/2.3 Input_output streams. Functions putchar() and getchar()/Feat 05/main.c
+++ tasks/2. Variables and arithmetic operations/2.3 Input_output streams. Functions putchar() and getchar()/Feat 05/main.c
@@ -1,16 +1,25 @@
 /**
  * The task description is at
  * @link{https://stepik.org/lesson/1191247/step/4?unit=1204218}
+ *
+ * The program must read the first three consecutive characters from the input
+ * stream using the 'getchar()' function, and then output them to the output
+ * stream in reverse order (from the last to the first) using the 'putchar()'
+ * function.
+ *
+ * P.S. In tests, it is guaranteed that at least three characters are present in
+ * the input stream.
+ *
  */
 #include <stdio.h>
 
 int main(void) {
-  char ch = 'u';
-  short sh = -55;
-  int var_i = 1024;
-  double var_d = 3.1415;
-
-  printf("%c %d %d %f", ch, sh, var_i, var_d);
+  int char0 = getchar();
+  int char1 = getchar();
+  int char2 = getchar();
+  putchar(char2);
+  putchar(char1);
+  putchar(char0);
 
   return 0;
 }
--- tasks/2. Variables and arithmetic operations/2.3 Input_output streams. Functions putchar() and getchar()/Feat 05/tests data.md
+++ tasks/2. Variables and arithmetic operations/2.3 Input_output streams. Functions putchar() and getchar()/Feat 05/tests data.md
@@ -1,1 +1,11 @@
-empty
+test #1
+input: abcd
+output: cba
+
+test #2
+input: efk
+output: kfe
+
+test #3
+input: 59g0ng 9
+output: g95
