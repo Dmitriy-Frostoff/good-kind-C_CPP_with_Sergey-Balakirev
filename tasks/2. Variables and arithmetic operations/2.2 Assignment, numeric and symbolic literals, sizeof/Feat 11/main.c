527e9611161bec9e8721ee284a36baf760a9c07e
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-29 19:56:58 +0300

    feat(task): add 2.2.11 task

---
+++ tasks/2. Variables and arithmetic operations/2.2 Assignment, numeric and symbolic literals, sizeof/Feat 11/main.c
@@ -0,0 +1,14 @@
+/**
+ * The task description is at
+ * @link{https://stepik.org/lesson/1191244/step/13?unit=1204215}
+ */
+int main(void) {
+  double test_var;
+  int res = sizeof(test_var);
+  /*
+   * макроопределение для тестирования
+   * (не убирать и должно идти непосредственно перед return 0)
+   */
+  __ASSERT_TESTS__
+  return 0;
+}
