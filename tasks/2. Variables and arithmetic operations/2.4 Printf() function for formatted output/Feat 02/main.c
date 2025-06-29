1d6abf31f792af4a14a6cc35ee5ad10af8d056ac
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-29 21:20:09 +0300

    chore(tasks): update 2.4.2 task description and tests data

--- tasks/2. Variables and arithmetic operations/2.4 Printf() function for formatted output/Feat 02/main.c
+++ tasks/2. Variables and arithmetic operations/2.4 Printf() function for formatted output/Feat 02/main.c
@@ -1,6 +1,11 @@
 /**
  * The task description is at
  * @link{https://stepik.org/lesson/1191245/step/5?thread=solutions&unit=1204216}
+ *
+ * The program should output all declared variables (in the order of their
+ * declaration) in one line separated by a space. Use the 'printf()' function to
+ * do this. The character variable must be output as a character.
+ *
  */
 #include <stdio.h>
 
--- tasks/2. Variables and arithmetic operations/2.4 Printf() function for formatted output/Feat 02/tests data.md
+++ tasks/2. Variables and arithmetic operations/2.4 Printf() function for formatted output/Feat 02/tests data.md
@@ -1,1 +1,13 @@
-empty
+Example:
+
+```C
+char ch = 'd';
+short sh = 13;
+int var_i = -121;
+double var_d = 0.3115;
+
+/*
+ * OUTPUT:
+ * d 13 -121 0.311500
+ */
+```
