ac6ce29bc6bdbc75498528a7826c4f4e4e3503d8
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-29 20:21:19 +0300

    feat(task): add 2.4.3 task

---
+++ tasks/2. Variables and arithmetic operations/2.4 Printf() function for formatted output/Feat 03/main.c
@@ -0,0 +1,16 @@
+/**
+ * The task description is at
+ * @link{https://stepik.org/lesson/1191247/step/5?unit=1204218}
+ */
+#include <stdio.h>
+
+int main(void) {
+  char byte = 65;
+  unsigned short height = 1055;
+  int distance = 1000000;
+  float pi = 3.1415f;
+
+  printf("%c\n%u\n%d\n%f\n", byte, height, distance, pi);
+
+  return 0;
+}
