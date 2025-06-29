5328076fbe6f6249b3bacab885107ce2dbf1f861
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-29 21:28:21 +0300

    chore(tasks): update 2.4.7 task description and tests data

--- tasks/2. Variables and arithmetic operations/2.4 Printf() function for formatted output/Feat 07/main.c
+++ tasks/2. Variables and arithmetic operations/2.4 Printf() function for formatted output/Feat 07/main.c
@@ -1,6 +1,19 @@
 /**
  * The task description is at
  * @link{https://stepik.org/lesson/1191247/step/9?unit=1204218}
+ *
+ * The program should print the values of variables using a single printf()
+ * function as follows:
+ *
+ * [ -123]
+ * [    6]
+ * [ 1024]
+ * (there are 5 characters inside each square bracket). For format output, use
+ * the flags of the specifiers.
+ *
+ * P.S. Do not use a space character in the format string of the printf()
+ * function.
+ *
  */
 #include <stdio.h>
 
