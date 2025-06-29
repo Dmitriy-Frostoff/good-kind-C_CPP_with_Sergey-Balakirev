d1176ebbf62d1f22f3615fd844e6015b5b108165
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-29 21:04:36 +0300

    chore(tasks): update 2.3.3 task description

--- tasks/2. Variables and arithmetic operations/2.3 Input_output streams. Functions putchar() and getchar()/Feat 03/main.c
+++ tasks/2. Variables and arithmetic operations/2.3 Input_output streams. Functions putchar() and getchar()/Feat 03/main.c
@@ -1,6 +1,11 @@
 /**
  * The task description is at
  * @link{https://stepik.org/lesson/1191245/step/5?thread=solutions&unit=1204216}
+ *
+ * The program must read one character (byte) from the input stream using the
+ * 'getchar()' function and output it to the output stream using the 'putchar()'
+ * function.
+ *
  */
 #include <stdio.h>
 
