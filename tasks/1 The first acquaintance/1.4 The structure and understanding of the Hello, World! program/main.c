0ec6bbff5257151332202ceeb9e4a73caac032d8
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-29 19:33:16 +0300

    feat(task): add 1.4 task

---
+++ tasks/1 The first acquaintance/1.4 The structure and understanding of the Hello, World! program/main.c
@@ -0,0 +1,10 @@
+/**
+ * The task description is at
+ * @link{https://stepik.org/lesson/1191239/step/9?thread=solutions&unit=1204210}
+ */
+#include <stdio.h>
+
+int main(void) {
+  printf("I love C");
+  return 0;
+}
