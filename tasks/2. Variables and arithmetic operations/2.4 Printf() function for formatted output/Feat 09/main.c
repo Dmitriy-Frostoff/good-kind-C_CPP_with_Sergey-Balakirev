4117fe94bc6dbea467ac2dcb027c10ed6ba0861f
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-29 20:29:54 +0300

    feat(task): add 2.4.9 task

---
+++ tasks/2. Variables and arithmetic operations/2.4 Printf() function for formatted output/Feat 09/main.c
@@ -0,0 +1,15 @@
+/**
+ * The task description is at
+ * @link{https://stepik.org/lesson/1191247/step/11?unit=1204218}
+ */
+#include <stdio.h>
+
+int main(void) {
+  unsigned char h = 11;
+  unsigned char m = 8;
+  unsigned char s = 1;
+
+  printf("%0.2u:%0.2u:%0.2u", h, m, s);
+
+  return 0;
+}
