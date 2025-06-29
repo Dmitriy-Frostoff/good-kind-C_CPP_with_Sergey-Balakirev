179acab48efc5784080493e883850187e78c4f1b
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-29 20:15:50 +0300

    feat(task): add 2.3.5 task

---
+++ tasks/2. Variables and arithmetic operations/2.3 Input_output streams. Functions putchar() and getchar()/Feat 05/main.c
@@ -0,0 +1,16 @@
+/**
+ * The task description is at
+ * @link{https://stepik.org/lesson/1191245/step/7?unit=1204216}
+ */
+#include <stdio.h>
+
+int main(void) {
+  int char0 = getchar();
+  int char1 = getchar();
+  int char2 = getchar();
+  putchar(char2);
+  putchar(char1);
+  putchar(char0);
+
+  return 0;
+}
