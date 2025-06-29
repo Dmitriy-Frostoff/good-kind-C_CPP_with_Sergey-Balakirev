88e703b7499454f5fccc5d14f30b2524492d5191
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-30 00:16:42 +0300

    feat(task): add 2.6.5 task

---
+++ tasks/2. Variables and arithmetic operations/2.6 Addition, subtraction, multiplication and division/Feat 05/main.c
@@ -0,0 +1,16 @@
+/**
+ * The task description is at
+ * @link{https://stepik.org/lesson/1191251/step/7?unit=1204222}
+ *
+ */
+#include <stdio.h>
+
+int main(void) {
+  int count = 0;
+  scanf("%d", &count);
+
+  int res = count + 1;
+  printf("%d", res);
+
+  return 0;
+}
