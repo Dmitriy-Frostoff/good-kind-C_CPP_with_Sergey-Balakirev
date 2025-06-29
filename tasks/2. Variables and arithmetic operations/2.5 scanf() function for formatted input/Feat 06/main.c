70fe08069b6b5556e5599a211786a6f37a7da22b
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-29 21:38:58 +0300

    feat(task): add 2.5.6 task

---
+++ tasks/2. Variables and arithmetic operations/2.5 scanf() function for formatted input/Feat 06/main.c
@@ -0,0 +1,16 @@
+/**
+ * The task description is at
+ * @link{https://stepik.org/lesson/1191249/step/8?unit=1204220}
+ *
+ */
+#include <stdio.h>
+
+int main(void) {
+  short char_1;
+  short char_2;
+
+  scanf("%hd; %hd", &char_1, &char_2);
+  printf("%d %d", char_1, char_2);
+
+  return 0;
+}
