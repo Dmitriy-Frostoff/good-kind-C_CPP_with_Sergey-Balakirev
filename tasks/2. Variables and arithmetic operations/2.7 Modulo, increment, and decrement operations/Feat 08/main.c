c36236c75fcf2c75d2b42ae405fb4fbe2f3008d7
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-30 00:51:33 +0300

    feat(task): add 2.7.8 task

---
+++ tasks/2. Variables and arithmetic operations/2.7 Modulo, increment, and decrement operations/Feat 08/main.c
@@ -0,0 +1,16 @@
+/**
+ * The task description is at
+ * @link{https://stepik.org/lesson/1191253/step/10?unit=1204224}
+ *
+ */
+
+#include <stdio.h>
+
+int main(void) {
+  double inputValue;
+
+  scanf("%lf", &inputValue);
+  printf("%.1f", --inputValue);
+
+  return 0;
+}
