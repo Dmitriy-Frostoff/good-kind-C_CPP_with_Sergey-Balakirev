c86ed772848eaaad43e5875f1469ac665b82e514
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-29 21:30:27 +0300

    chore(tasks): update 2.4.8 task description and tests data

--- tasks/2. Variables and arithmetic operations/2.4 Printf() function for formatted output/Feat 08/main.c
+++ tasks/2. Variables and arithmetic operations/2.4 Printf() function for formatted output/Feat 08/main.c
@@ -1,6 +1,10 @@
 /**
  * The task description is at
  * @link{https://stepik.org/lesson/1191247/step/10?unit=1204218}
+ *
+ * The program must use the 'printf()' function to output the value of a real
+ * variable with an accuracy of one thousandth (three decimal places).
+ *
  */
 #include <stdio.h>
 
