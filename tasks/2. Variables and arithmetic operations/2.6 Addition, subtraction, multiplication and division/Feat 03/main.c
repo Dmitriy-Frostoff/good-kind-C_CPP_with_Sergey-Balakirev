14ef5420fc62c9e82812c8f6c2be9949b29b61f9
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-30 00:12:14 +0300

    feat(task): add 2.6.3 task

---
+++ tasks/2. Variables and arithmetic operations/2.6 Addition, subtraction, multiplication and division/Feat 03/main.c
@@ -0,0 +1,13 @@
+/**
+ * The task description is at
+ * @link{https://stepik.org/lesson/1191251/step/5?unit=1204222}
+ *
+ */
+#include <stdio.h>
+
+int main(void) {
+  double res = 6 * ((1.0 / 3.0) * (1.0 / 3.0)) - 15 * (1.0 / 3.0);
+  printf("%.2f", res);
+
+  return 0;
+}
