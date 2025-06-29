8920de514acaa5b436a441038cbcc26ad87070dd
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-29 21:01:21 +0300

    chore(tasks): update 2.2.4 task description

--- tasks/2. Variables and arithmetic operations/2.2 Assignment, numeric and symbolic literals, sizeof/Feat 04/main.c
+++ tasks/2. Variables and arithmetic operations/2.2 Assignment, numeric and symbolic literals, sizeof/Feat 04/main.c
@@ -1,6 +1,11 @@
 /**
  * The task description is at
  * @link{https://stepik.org/lesson/1191244/step/6?thread=solutions&unit=1204215}
+ *
+ * Declare two real variables of type 'double' with the names 'var_d1',
+ * 'var_d2'. After that (with the next operator) assign them the value 0.5 along
+ * the chain, using a construction of the form a = b = c = d
+ *
  */
 int main(void) {
   double var_d1;
