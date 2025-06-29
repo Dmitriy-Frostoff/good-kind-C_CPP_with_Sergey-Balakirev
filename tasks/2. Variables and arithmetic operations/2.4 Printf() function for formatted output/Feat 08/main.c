a1daaf1a414c1d2c65b2e6d4432ee76f89630374
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-29 20:27:45 +0300

    feat(task): add 2.4.8 task

---
+++ tasks/2. Variables and arithmetic operations/2.4 Printf() function for formatted output/Feat 08/main.c
@@ -0,0 +1,13 @@
+/**
+ * The task description is at
+ * @link{https://stepik.org/lesson/1191247/step/10?unit=1204218}
+ */
+#include <stdio.h>
+
+int main(void) {
+  double d = 546e-3;
+
+  printf("%.3f", d);
+
+  return 0;
+}
