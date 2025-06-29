5e8299a414aaa4c079aec929ee634a1fba85c8b9
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-29 21:34:00 +0300

    chore(tasks): update 2.4.10 task description and tests data

--- tasks/2. Variables and arithmetic operations/2.4 Printf() function for formatted output/Feat 10/main.c
+++ tasks/2. Variables and arithmetic operations/2.4 Printf() function for formatted output/Feat 10/main.c
@@ -1,6 +1,19 @@
 /**
  * The task description is at
  * @link{https://stepik.org/lesson/1191247/step/12?unit=1204218}
+ *
+ * The program should use the printf() function to output the current time
+ * stored in variables: h - hours; m - minutes; s - seconds in hexadecimal
+ * format with the prefix 0x output.:
+ *
+ * 0xhh:0xmm:0xss
+ *
+ * For example, for h = 12, m = 3, s = 9 it should be:
+ *
+ * 0x0c:0x03:0x09
+ *
+ * (note the insignificant zero if the number is less than 16).
+ *
  */
 #include <stdio.h>
 
