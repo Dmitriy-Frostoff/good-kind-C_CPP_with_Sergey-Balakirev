fb5abb58f39afb3ccd84c00591a18c6c519d47ca
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-06-29 19:54:28 +0300

    feat(task): add 2.2.4 task

---
+++ tasks/2. Variables and arithmetic operations/2.2 Assignment, numeric and symbolic literals, sizeof/Feat 04/main.c
@@ -0,0 +1,15 @@
+/**
+ * The task description is at
+ * @link{https://stepik.org/lesson/1191244/step/6?thread=solutions&unit=1204215}
+ */
+int main(void) {
+  double var_d1;
+  double var_d2;
+  var_d1 = var_d2 = 0.5;
+  /*
+   * макроопределение для тестирования
+   * (не убирать и должно идти непосредственно перед return 0)
+   */
+  __ASSERT_TESTS__
+  return 0;
+}
