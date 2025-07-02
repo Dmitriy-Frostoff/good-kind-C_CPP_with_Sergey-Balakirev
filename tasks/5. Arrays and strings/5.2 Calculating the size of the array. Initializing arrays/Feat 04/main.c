2b28caaf3bdbec62fbc2c65e66e9fbced9cff5bd
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-07-03 02:01:50 +0300

    feat(task): add 5.2.4 task

---
+++ tasks/5. Arrays and strings/5.2 Calculating the size of the array. Initializing arrays/Feat 04/main.c
@@ -0,0 +1,25 @@
+/**
+ * The task description is at
+ * @link{https://stepik.org/lesson/1191292/step/6?unit=1204280}
+ *
+ * There's an array with the name 'pows' that consist of element of type float.
+ * In the main() function, output the values of its elements to the console in
+ * one line separated by a space in reverse order, accurate to tenths.
+ *
+ */
+
+#include <stddef.h>
+#include <stdio.h>
+
+int main(void) {
+  // size_t => char
+  // do not use !!!'unsigned'!!! Or one will get the infinite loop
+  // in the further cycle (i < 0 but unsigned always >= 0)
+  size_t pows_length = sizeof(pows) / sizeof(*pows);
+
+  for (size_t i = pows_length - 1; i >= 0; i -= 1) {
+    printf("%.1f ", pows[i]);
+  }
+
+  return 0;
+}
