d64c9a8b951bbc5466fb51cc555c76e692dd18c1
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-07-03 21:34:27 +0300

    feat(task): add 6.5.1 task

---
+++ tasks/6. Functions/6.5 Pointers to a function. Function as a parameter (callback)/Feat 01/main.c
@@ -0,0 +1,13 @@
+/**
+ * The task description is at
+ * @link{https://stepik.org/lesson/1191306/step/3?unit=1204294}
+ *
+ */
+
+#include <stdio.h>
+
+double (*ptr_func)(int, int);
+
+int main(void) {
+  return 0;
+}
