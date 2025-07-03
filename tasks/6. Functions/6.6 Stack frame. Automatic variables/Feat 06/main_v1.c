457174f1c2a35925870404022890bc0d89c934bf
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-07-03 21:55:27 +0300

    feat(task): add another decision of the 6.6.6 task

---
+++ tasks/6. Functions/6.6 Stack frame. Automatic variables/Feat 06/main_v1.c
@@ -0,0 +1,154 @@
+/**
+ * The task description is at
+ * @link{https://stepik.org/lesson/1191307/step/8?unit=1204295}
+ *
+ */
+
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+#define STR_SIZE 100
+#define STR_TEMPLATE_SIZE 20
+
+int get_string_data(char *str, size_t sizeof_str);
+int remove_str_last_n(char *str);
+void strip_str(char *str, const char *str_template);
+
+int main(void) {
+  char str[STR_SIZE] = {0};
+  char str2[STR_TEMPLATE_SIZE] = ".,-!?";
+
+  // get the string from the input
+  if (get_string_data(str, sizeof(str)) == 1) {
+    return 1;
+  }
+
+  // remove '\n' from the end of string
+  if (remove_str_last_n(str)) {
+    return 1;
+  }
+
+  // mutate the @link{str} => remove all @link{str2} chars
+  strip_str(str, str2);
+
+  // print to the console str
+  puts(str);
+
+  return 0;
+}
+
+/**
+ *  @brief Get the string data from the input
+ *
+ *  @param {char *} str - string data
+ *  @param {size_t} sizeof_str - string size
+ *
+ *  @return {int} - 0, everything OK
+ *  @throw if empty data was produced and retrun exit code 1
+ *
+ *  @example
+ *    char str[20] = {0};
+ *      get_string_data(str, sizeof(str)) =>
+ *      INPUT:
+ *      "Hello world!" => 0
+ */
+int get_string_data(char *str, size_t sizeof_str) {
+  if (fgets(str, sizeof_str - 1, stdin) == NULL) {
+    puts("Error: empty string data");
+    return 1;
+  }
+
+  return 0;
+}
+
+/**
+ *  @brief Remove last '\n' from the string
+ *
+ *  @param {char *} str - pointer to the valid string
+ *
+ *  @return {int} - 0, everything is OK
+ *  @throw if empty string pointer was produced and returns exit code 1
+ *
+ *  @example
+ *    char str[20] = {H, e, l, l, o,  , w, o, r, l, d, !, \n, \0};
+ *      remove_str_last_n(str) => 0
+ *      str{H, e, l, l, o,  , w, o, r, l, d, !, \0}
+ */
+int remove_str_last_n(char *str) {
+  if (str == NULL) {
+    puts("Error: empty string pointer");
+    return 1;
+  }
+
+  char *ptr_n = strrchr(str, '\n');
+  if (ptr_n != NULL) {
+    *ptr_n = '\0';
+  }
+
+  return 0;
+}
+
+/**
+ *  @brief Remove chars from the @link{str_template} in the @link{str}
+ *
+ *  @note !Impure function! It mutates the original @link{str} !
+ *
+ *  Under the hood @callback{strip_str} replaces all the chars from the
+ *    @link{str_template} in the @link{str} with '\0' char, and than collapse
+ *    '\0' (exclude last in the @link{str}) with next chars
+ *
+ *  @param {char *} str - string to mutate (remove chars)
+ *  @param {const char *} str_template - template string with chars for removing
+ *
+ *  @return {void} - just mutates the original @link{str}
+ *
+ *  @example
+ *    char str[100] = "!sc-lib@list.ru?";
+ *    char str_template[20] = ".,-!?";
+ *      strip_str(str, str_template) => void
+ *      str{'s', 'c', 'l', 'i', 'b', '@', 'l', 'i', 's', 't', 'r', 'u', \0}
+ */
+void strip_str(char *str, const char *str_template) {
+  size_t str_len = strlen(str);
+  // last char is '\0'
+  size_t str_last_index = str_len + 1;
+  char is_zero_met = 0;
+  //  replace all the chars from @link{str_template} in the @link{str}
+  char *ptr_search = strpbrk(str, str_template);
+  char *ptr_last_search = strrchr(str, '\0');
+  while (ptr_search != NULL && ptr_search < ptr_last_search) {
+    // replace the found char from the @link{str_template}
+    *ptr_search = '\0';
+    // go to the next match
+    ptr_search = strpbrk(ptr_search + 1, str_template);
+  }
+
+  // reset pointer
+  ptr_search = NULL;
+
+  // collapse all the '\0' excluding last one
+  for (int i = 0, j = i + 1; i <= str_len; i += 1, j += 1) {
+    // current char is '\0' ?=> go further till char is not '\0'
+    if (str[i] == '\0') {
+      str_last_index -= 1;
+      is_zero_met = 1;
+      // skip '\0' str_last_index will protect correct string ending
+      while (str[j] == '\0') {
+        j += 1;
+      }
+    }
+
+    if (is_zero_met) {
+      // skip '\0' str_last_index will protect correct string ending
+      while (str[j] == '\0') {
+        j += 1;
+      }
+      // move @link{str} chars to the left (replace '\0')
+      str[i] = str[j];
+    }
+  }
+
+  // set correct '\0'
+  str[str_last_index] = '\0';
+}
