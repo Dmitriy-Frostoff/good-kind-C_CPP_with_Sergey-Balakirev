f7a4e0b0fdfcffb84075bfb917c87d51205726f4
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-07-04 17:51:05 +0300

    refactor(task): break 6.6.3 task decision into modules

---
+++ tasks/6. Functions/6.6 Stack frame. Automatic variables/Feat 03/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* -2 and 10 are defined in the task requirements*/
+#define RANGE_MIN -2
+#define RANGE_MAX 10
+
+#endif
---
+++ tasks/6. Functions/6.6 Stack frame. Automatic variables/Feat 03/utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "./config.h"
+
+double norm(int a, int b);
+
+#endif
