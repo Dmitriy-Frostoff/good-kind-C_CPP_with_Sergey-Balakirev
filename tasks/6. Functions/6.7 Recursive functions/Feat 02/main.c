35c0adf3b1c002fd0ddad6b2d667fc0a8b9124b9
Dmitriy Frostoff <dmitriy.frostoff@yandex.ru>
2025-07-04 18:21:02 +0300

    refactor(task): break 6.7.2 task decision into modules

--- tasks/6. Functions/6.7 Recursive functions/Feat 02/main.c
+++ tasks/6. Functions/6.7 Recursive functions/Feat 02/main.c
@@ -4,43 +4,11 @@
  *
  */
 
-#include <stdio.h>
-
-#define MAX_RECURSION 4
-
-void recursive(int num);
+#include "./config.h"
+#include "./utils.h"
 
 int main(void) {
   recursive(1);
 
   return 0;
-}
-
-/**
- *  @brief Print (space separated) to the terminal recursively backwards nums
- *  from MAX_RECURSION to 1 [MAX_RECURSION; 1]
- *
- *  @note !Impure function! Depends on globals macros MAX_RECURSION !
- *
- *  @param {int} num - start num
- *
- *  @return {void} - nothing
- *
- *  @example
- *    recursive(1) => void
- *    OUTPUT:
- *    4 3 2 1
- */
-void recursive(int num) {
-  if (num < MAX_RECURSION) {
-    recursive(num + 1);
-  }
-
-  // remove trailing space
-  if (num == 1) {
-    printf("%d", num);
-    return;
-  }
-
-  printf("%d ", num);
 }
